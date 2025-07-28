85187252139ca1d6b2240ff2ea5a701849f84ff6
wagner-sm <wsmmetal@outlook.com>
2025-07-28 14:11:59 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Importar Bootstrap JavaScript
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
